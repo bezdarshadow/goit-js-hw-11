da7d6d72f26192524ae80feaac2b9d4b11f31008
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-25 16:26:45 +0200

    Добавил сборку + временное состояние файлов

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class PixabayAPI {
+    #BASE_URL = 'https://pixabay.com/api/';
+    #API_KEY = '25406672-b5207d9f82ab532bfb9f3b93a';
+
+    constructor(keyword){
+        this.page = 1;
+        this.keyword = keyword;
+    }
+
+
+    fetchPhotos(){
+        
+    }
+
+}

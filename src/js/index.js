217993982fde11b5a318bdbf546bfe77cdeacb3a
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-26 07:55:53 +0200

    1
    12

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
 import '../css/styles.css';
+import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import galleryCardTemplate from '../templates/galleryItemTemplate.hbs'
+
+import { PixabayAPI } from './pixabay-api';
+
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+
+const pixabayApi = new PixabayAPI();
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormSubmit (event){
+    event.preventDefault();
+
+    const keyword = event.currentTarget.elements['searchQuery'].value;
+
+    if (keyword.trim() === '') {
+      return;
+    }
+
+    pixabayApi.page = 1;
+    pixabayApi.searchQuery = keyword;
+   
+    pixabayApi.fetchPhotos().then(data =>{
+        console.log(data)
+        galleryEl.insertAdjacentHTML('beforeend', galleryCardTemplate(data))
+    })
+
+}
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,34 @@
 'use strict';
 
-class PixabayAPI {
+import axios from "axios";
+
+export class PixabayAPI {
     #BASE_URL = 'https://pixabay.com/api/';
     #API_KEY = '25406672-b5207d9f82ab532bfb9f3b93a';
 
-    constructor(keyword){
+    constructor(keyword = null){
         this.page = 1;
-        this.keyword = keyword;
+        this.searchQuery = keyword;
     }
 
 
     fetchPhotos(){
-        
+        return axios.get(`${this.#BASE_URL}`, {
+            params:{
+                key: this.#API_KEY,
+                q: this.searchQuery,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: this.page,
+                per_page: 20,
+            },
+        }).then(response => {
+            return response.data
+        }).catch(error => {
+            console.log(error)
+        })
     }
 
 }
+
